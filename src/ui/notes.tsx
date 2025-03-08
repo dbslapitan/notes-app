fc8e104a2f09870c49894fd5bd4e1b91df7d3e8b
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-08 00:41:29 -0500

    add resize event listener to change scroll height on event trigger

--- src/ui/notes.tsx
+++ src/ui/notes.tsx
@@ -8,18 +8,37 @@ import { RefObject, useEffect, useRef, useState } from "react";
 export default function Notes({notes, bottomRef}: {notes: INote[], bottomRef: RefObject<null | HTMLDivElement>}){
 
   const scrollRef = useRef<null | HTMLDivElement>(null);
+  const initialLoadRef = useRef(false);
 
   useEffect(() => {
-    const top = (scrollRef.current as HTMLDivElement)?.getBoundingClientRect().top;
-    const bottom = (bottomRef.current as HTMLDivElement)?.getBoundingClientRect().height;
-    (scrollRef.current as HTMLDivElement).setAttribute("style", `height: ${window.innerHeight - top - bottom}px`);
+    const setScrollHeight = () => {
+      const top = (scrollRef.current as HTMLDivElement)?.getBoundingClientRect().top;
+      const bottom = (bottomRef.current as HTMLDivElement)?.getBoundingClientRect().height;
+      (scrollRef.current as HTMLDivElement).setAttribute("style", `height: ${window.innerHeight - top - bottom}px`);
+    }
+
+    if(!initialLoadRef.current){
+      window.addEventListener("resize", setScrollHeight);
+    }
+
+    initialLoadRef.current = true;
+
+    return () => {
+      window.removeEventListener("resize", setScrollHeight);
+    }
   });
 
   return(
     <>
       <h1 className={`${text["preset-1"]}`}>Notes</h1>
       <ScrollArea ref={scrollRef} className="h-0">
-      
+        <ul>
+          {
+            <li>
+              
+            </li>
+          }
+        </ul>
       </ScrollArea>
     </>
   );
